f5dac9700772df09262e51f1823f29063a176523
tchenrique <tchenrique@msn.com>
2018-01-02 12:05:04 -0200

    Latest commit

--- public/js/angular/controllers/detalhesContatoCtrl.js
+++ public/js/angular/controllers/detalhesContatoCtrl.js
@@ -1,3 +1,4 @@
 angular.module('listaTelefonica').controller('detalhesContatoCtrl', function($scope, $routeParams, contato) {
+    $scope.app = 'Detalhes do Contato';
     $scope.contato = contato.data;
 });
